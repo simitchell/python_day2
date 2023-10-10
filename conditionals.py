8eadd2623eddaa8283e79033ddc03ddeda841441
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-10 14:53:53 -0400

    added string to list

--- conditionals.py
+++ conditionals.py
@@ -1,16 +1,27 @@
 import random
 
 def guessing_game():
-    guess = input('Select a number between 1 and 10\n')
-    magic_number = random.randrange(1, 10, 1)
-    print("Your number is {}.".format(guess))
-    if (int(guess) > 12 ):
-        print("You're out of range, try again homie.")
-    elif (int(guess) == magic_number):
+    
+    try:
+
+        guess = int(input('Select a number between 1 and 10\n'))
+        magic_number = random.randrange(1, 10, 1)
+        # magic_number = 4
+        print("Your number is {}.".format(guess))
+        while guess != magic_number:
+            
+            if (guess > 12 ):
+                print("You're out of range, try again homie.")
+                guess = int(input('Select a number between 1 and 10\n'))
+            else:
+                print("Try Again womp")
+                print("The magic number was {}.\n".format(magic_number))
+                guess = int(input('Select a number between 1 and 10\n'))
+        
         print("Are you a MIND READER??")
         print("The magic number was {}.\n".format(magic_number))
-    else:
-        print("Try Again womp")
-        print("The magic number was {}.\n".format(magic_number))
+    except:
+        print("Don't type a string, ya dummy")
+        guessing_game()
 
 guessing_game()
--- lists.py
+++ lists.py
@@ -1,7 +1,19 @@
-def reverseList():
-    numbers = [1, 2, 3, 4, 5, 99, 2600, 300]
-    print(numbers)
-    reversed_list = reversed(numbers)
+# def reverseList():
+#     numbers = [1, 2, 3, 4, 5, 99, 2600, 300]
+#     print(numbers)
+#     reversed_list = reversed(numbers)
+#     print(list(reversed_list))
+
+# reverseList()
+
+def stringToList():
+    myString = "teriyaki"
+    # print(myString)
+    myList = []
+    for char in myString:
+        myList.append(char)
+    print(myList)
+    reversed_list = reversed(myList)
     print(list(reversed_list))
-    
-reverseList()
+
+stringToList()
