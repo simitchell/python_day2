5797a7a6650ac2df98a16f442c5072aaf5ead4e5
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-10 16:47:59 -0400

    implemented more logic to check for criteria of myString

--- loops.py
+++ loops.py
@@ -1,7 +1,26 @@
-def start_end():
-    start = int(input("What number shall we begin with?\n"))
-    end = int(input("Awesome.  We'll start with {}.  What shall we end with? \n".format(start)))
-    for x in range(start, end, 1):
-        print(x)
+# def start_end():
+#     start = int(input("What number shall we begin with?\n"))
+#     end = int(input("Awesome.  We'll start with {}.  What shall we end with? \n".format(start)))
+#     for x in range(start, end, 1):
+#         print(x)
 
-start_end()
+# start_end()
+
+import re
+
+def multi_condition():
+    myString = input("Give me two words, they must total 16 characters or more. \n")
+    counter = 0
+    if (len(myString) < 16):
+        print("Not enough characters, my dude.\n")
+        multi_condition()
+    elif (bool(re.search(r"\s", myString))):
+        print("There is a space in the string")
+    else:
+        while counter < len(myString):
+            for x in myString:
+                counter += 1
+            print(counter)
+    
+
+multi_condition()
